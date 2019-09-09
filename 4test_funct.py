14042cceca5baf7b028baeed0ff504a0861bfd91
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-09-09 21:12:08 +0200

    Create 4test_funct.py

---
+++ 4test_funct.py
@@ -0,0 +1,12 @@
+import funct
+import pytest
+@pytest.mark.parametrize('num1, num2, result',
+                         [
+                             (7,3,10),
+                             ('Hello', ' World', 'Hello World'),
+                             (10.5, 25.5, 36)
+                         ]
+                         )
+
+def test_add(num1, num2, result):
+    assert funct.add(num1, num2) == result
